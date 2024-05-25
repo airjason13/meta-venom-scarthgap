b9070b0d840d80df86b173d32fc51b4581016a58
airjason13 <airjason13@gmail.com>
2024-05-25 18:36:48 +0800

    1. submodle test

--- setup_submodule.sh
+++ setup_submodule.sh
@@ -2,11 +2,11 @@
 
 git checkout main
 
-git submodule add https://github.com/airjason13/pyLed_Server.git recipes-apps/ledserver/ledserver/pyLed_Server/
-git submodule add https://chromium.googlesource.com/libyuv/libyuv.git recipes-apps/libyuv/libyuv/libyuv
-git submodule add https://github.com/aircrack-ng/rtl8812au.git recipes-bsp/rtl8812au/rtl8812au
-git submodule add https://github.com/airjason13/pyLed_test_server.git recipes-apps/ledtester/ledtester/pyLedTester
-git submodule add https://github.com/airjason13/pyLedRATool.git recipes-apps/ledratool/ledratool/pyLedRATool
+git submodule add  --force https://github.com/airjason13/pyLed_Server.git recipes-apps/ledserver/ledserver/pyLed_Server/
+git submodule add --force https://chromium.googlesource.com/libyuv/libyuv.git recipes-apps/libyuv/libyuv/libyuv
+git submodule add --force https://github.com/aircrack-ng/rtl8812au.git recipes-bsp/rtl8812au/rtl8812au
+git submodule add --force https://github.com/airjason13/pyLed_test_server.git recipes-apps/ledtester/ledtester/pyLedTester
+git submodule add --force https://github.com/airjason13/pyLedRATool.git recipes-apps/ledratool/ledratool/pyLedRATool
 
 
 
